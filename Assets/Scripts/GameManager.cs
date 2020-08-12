a19184057ac8594d862a134f002fd4227f5b21dc
jiayaoguang <jiayaoguang@coolfishGames.com>
2020-08-12 11:17:34 +0800

    init commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using DotNetty.Handlers.Logging;
+using DotNetty.Transport.Bootstrapping;
+using DotNetty.Transport.Channels;
+using DotNetty.Transport.Channels.Sockets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Bootstrap bootstrap = new Bootstrap();
+        IEventLoopGroup eventLoopGroup = new MultithreadEventLoopGroup(1);
+        bootstrap.Group(eventLoopGroup);
+        bootstrap.Channel<TcpServerSocketChannel>();// 设置通道模式为TcpSocket
+        bootstrap.Option(ChannelOption.SoBacklog, 1024);
+
+        bootstrap.Handler(new LoggingHandler(LogLevel.WARN));
+
+        bootstrap.Handler(new MyChannelHandler());
+
+        bootstrap.Option(ChannelOption.SoKeepalive, true);//是否启用心跳保活机制
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MyChannelHandler.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using DotNetty.Transport.Channels;
+
+public class MyChannelHandler : IChannelHandler
+{
+    public Task BindAsync(IChannelHandlerContext context, EndPoint localAddress)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ChannelActive(IChannelHandlerContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ChannelInactive(IChannelHandlerContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ChannelRead(IChannelHandlerContext context, object message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ChannelReadComplete(IChannelHandlerContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ChannelRegistered(IChannelHandlerContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ChannelUnregistered(IChannelHandlerContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ChannelWritabilityChanged(IChannelHandlerContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task CloseAsync(IChannelHandlerContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task ConnectAsync(IChannelHandlerContext context, EndPoint remoteAddress, EndPoint localAddress)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeregisterAsync(IChannelHandlerContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DisconnectAsync(IChannelHandlerContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ExceptionCaught(IChannelHandlerContext context, Exception exception)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Flush(IChannelHandlerContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void HandlerAdded(IChannelHandlerContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void HandlerRemoved(IChannelHandlerContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Read(IChannelHandlerContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UserEventTriggered(IChannelHandlerContext context, object evt)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task WriteAsync(IChannelHandlerContext context, object message)
+    {
+        throw new NotImplementedException();
+    }
+}
