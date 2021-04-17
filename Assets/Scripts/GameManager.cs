c46259d39a3e4ed9ebf7153736e445caf0250586
jiayaoguang <jiayaoguang@coolfishGames.com>
2021-04-17 22:55:58 +0800

    tcpclient

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,25 @@
-﻿using DotNetty.Handlers.Logging;
+﻿/*using DotNetty.Handlers.Logging;
 using DotNetty.Transport.Bootstrapping;
 using DotNetty.Transport.Channels;
-using DotNetty.Transport.Channels.Sockets;
+using DotNetty.Transport.Channels.Sockets;*/
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    /*    private Task<IChannel> channelTask;
+    */    // Start is called before the first frame update
+
+
+    long lastSnedTime;
+
     void Start()
     {
-        Bootstrap bootstrap = new Bootstrap();
+        /*Bootstrap bootstrap = new Bootstrap();
         IEventLoopGroup eventLoopGroup = new MultithreadEventLoopGroup(1);
         bootstrap.Group(eventLoopGroup);
         bootstrap.Channel<TcpServerSocketChannel>();// 设置通道模式为TcpSocket
@@ -19,17 +27,46 @@ public class GameManager : MonoBehaviour
 
         bootstrap.Handler(new LoggingHandler(LogLevel.WARN));
 
-        bootstrap.Handler(new MyChannelHandler());
+        bootstrap.Handler(new MyChannelHandler<System.Object>());
 
         bootstrap.Option(ChannelOption.SoKeepalive, true);//是否启用心跳保活机制
 
+        channelTask = bootstrap.ConnectAsync("127.0.0.1",8080);*/
 
+
+        TcpClient.Connect();
+
+        Console.WriteLine("1111111111111111");
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        TimeSpan ts = DateTime.Now - new DateTime(1970, 1, 1, 0, 0, 0, 0);
         
+        long time = Convert.ToInt64(ts.TotalSeconds);
+
+
+
+        if (lastSnedTime + 3L > time) {
+            return;
+        }
+        lastSnedTime = time;
+        TcpClient.Send(108,"{ \"type\":111}");
+
+       // Debug.Log("send===========");
+
+
     }
+
+
+    public static void Main(string[] s) {
+
+        
+
+        Console.WriteLine("1111111111111111");
+
+    }
+
 }
--- Assets/Scripts/MyChannelHandler.cs
+++ Assets/Scripts/MyChannelHandler.cs
@@ -1,102 +1,13 @@
-﻿using System;
+﻿/*using System;
 using System.Net;
 using System.Threading.Tasks;
 using DotNetty.Transport.Channels;
+using UnityEngine;
 
-public class MyChannelHandler : IChannelHandler
+public class MyChannelHandler<Object> : SimpleChannelInboundHandler<Object>
 {
-    public Task BindAsync(IChannelHandlerContext context, EndPoint localAddress)
+    protected override void ChannelRead0(IChannelHandlerContext ctx, Object msg)
     {
         throw new NotImplementedException();
     }
-
-    public void ChannelActive(IChannelHandlerContext context)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void ChannelInactive(IChannelHandlerContext context)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void ChannelRead(IChannelHandlerContext context, object message)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void ChannelReadComplete(IChannelHandlerContext context)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void ChannelRegistered(IChannelHandlerContext context)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void ChannelUnregistered(IChannelHandlerContext context)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void ChannelWritabilityChanged(IChannelHandlerContext context)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task CloseAsync(IChannelHandlerContext context)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task ConnectAsync(IChannelHandlerContext context, EndPoint remoteAddress, EndPoint localAddress)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task DeregisterAsync(IChannelHandlerContext context)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task DisconnectAsync(IChannelHandlerContext context)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void ExceptionCaught(IChannelHandlerContext context, Exception exception)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Flush(IChannelHandlerContext context)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void HandlerAdded(IChannelHandlerContext context)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void HandlerRemoved(IChannelHandlerContext context)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Read(IChannelHandlerContext context)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void UserEventTriggered(IChannelHandlerContext context, object evt)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task WriteAsync(IChannelHandlerContext context, object message)
-    {
-        throw new NotImplementedException();
-    }
-}
+}*/
